ec9e130dfce75c58bf60c489e043e4198235c82a
Giorgia10 <giorgia.diplacido@aesys.tech>
2020-04-23 13:14:12 +0200

    Various changes

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
 const credential = {
-    email: "eve.holt@reqres.in",
+  email: "eve.holt@reqres.in",
+  password: "abcd1234",
+};
+  
+function validateForm() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  if (email == "" && password == "") {
+    alert("Email e password obbligatori");
+    return false;
+  } else {
+    http(email, password);
+  }
+
+  if (password == "") {
+    alert("Password errata");
+    return false;
+  } else {
+    http(password);
+    window.location.href="homepage.html";
+  }
+}
+
+function logout() {
+  localStorage.clear();
+  window.location.href="loginpage.html";
+}
+  
+  
+function http(email) {
+  fetch("https://reqres.in/api/login", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    email: email,
     password: "abcd1234",
-  };
-  
-  function validateForm() {
-    var email = document.getElementById("email").value;
-    if (email == "") {
-      alert("E-mail errata o obbligatoria!");
-      return false;
-    } else {
-      http(email);
-    }
-  }
-  
-  function http(email) {
-    fetch("https://reqres.in/api/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: email,
-        password: "abcd1234",
-      }),
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log('token -> ',data.token);
-      });
-  }
+  }),
+})
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log('token -> ',data.token);
+  });
+}
